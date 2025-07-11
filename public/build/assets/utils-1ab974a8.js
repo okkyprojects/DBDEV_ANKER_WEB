90d752375d361d725e32d68513ee7023e86de9fc
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2025-07-11 15:22:56 +0700

    feat cart

--- public/build/assets/utils-1ab974a8.js
+++
@@ -1,1 +0,0 @@
-const t=r=>new Intl.NumberFormat("id-ID",{style:"currency",currency:"IDR",minimumFractionDigits:0}).format(r);export{t as f};
---
+++ public/build/assets/utils-31543150.js
@@ -0,0 +1,1 @@
+const i=r=>{const t=Math.round(Number(r)||0);return new Intl.NumberFormat("id-ID",{style:"currency",currency:"IDR",minimumFractionDigits:0,maximumFractionDigits:0}).format(t)};export{i as f};
