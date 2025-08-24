ae622c15ae694a08d11196c468909d40b3401345
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2025-08-24 19:24:32 +0700

    feat cronjob

---
+++ app/Console/Commands/ExpireTransaction.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Transaction;
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+
+class ExpireTransaction extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:expire-transaction';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $count = Transaction::where('status', 0)
+            ->where('expired_at', '<', Carbon::now())
+            ->update([
+                'status' => 6,
+                'updated_at' => now()
+            ]);
+
+        $this->info("Total transaksi expired: {$count}");
+    }
+}
