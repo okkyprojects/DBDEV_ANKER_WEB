c3dbba2f14d5b4f945b0c155c32ff45ca4476bbc
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2025-09-01 21:57:22 +0700

    feat bulk

--- resources/js/Components/Sidebar/Sidebar.jsx
+++ resources/js/Components/Sidebar/Sidebar.jsx
@@ -65,12 +65,12 @@ const sidebarMenu = {
             text: "Brand",
             url: "/master/brand",
         },
-        {
-            id: 8,
-            icon: <PiCirclesFourLight size={21} className="mr-4" />,
-            text: "Banner",
-            url: "/master/banner",
-        },
+        // {
+        //     id: 8,
+        //     icon: <PiCirclesFourLight size={21} className="mr-4" />,
+        //     text: "Banner",
+        //     url: "/master/banner",
+        // },
         {
             id: 9,
             icon: <PiCirclesFourLight size={21} className="mr-4" />,
