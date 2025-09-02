ae77ee4f889bc5fcd3820d2451fea1a7758c4cde
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2025-09-02 12:47:27 +0700

    feat sort best_seller

--- app/Http/Repositories/ProductRepository.php
+++ app/Http/Repositories/ProductRepository.php
@@ -8,6 +8,7 @@ use App\Models\Product;
 use App\Models\Variant;
 use App\Traits\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
