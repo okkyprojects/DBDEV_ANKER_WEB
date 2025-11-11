0a89773bab5db2d6e65a93946e44614cbdb5578b
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2025-11-11 19:49:21 +0700

    fix deleted

---
+++ public/build/assets/AuthLayout-ab8fa70e.js
@@ -0,0 +1,32 @@
+import{j as a,a as e}from"./app-26121b83.js";import{S as r,P as s,A as d,a as t}from"./pagination-d2be84a7.js";function c({children:l}){return a("div",{className:"min-h-screen flex font-dinnext text-neutral-900",children:[a("div",{className:"hidden lg:flex w-1/2 relative",children:[a(r,{spaceBetween:0,slidesPerView:1,loop:!0,autoplay:{delay:5e3},pagination:{clickable:!0,renderBullet:(i,n)=>`<span class="${n} ${i===0?"w-10":"w-5"} h-1 mx-1 rounded-full bg-red-600 inline-block transition-all"></span>`},modules:[s,d],className:"w-full h-full",children:[e(t,{children:e("div",{className:"w-full h-full bg-cover bg-center relative",style:{backgroundImage:"url(/images/auth/1.svg)"},children:a("div",{className:"absolute bottom-20 px-10",children:[e("p",{className:"text-white text-3xl font-medium",children:"Temukan Suara Impianmu!"}),e("p",{className:"text-white text-sm mt-2",children:"Jelajahi berbagai earphone, headset, speaker, dan perangkat audio canggih lainnya untuk pengalaman mendengarkan yang luar biasa."})]})})}),e(t,{children:e("div",{className:"w-full h-full bg-cover bg-center relative",style:{backgroundImage:"url(/images/auth/2.svg)"},children:a("div",{className:"absolute bottom-20 px-10",children:[e("p",{className:"text-white text-3xl font-medium",children:"Pilihan Audio Terbaik Ada di Sini"}),e("p",{className:"text-white text-sm mt-2",children:"Hadirkan kualitas suara jernih dan detail dari brand-brand ternama, siap memenuhi gaya dan kebutuhanmu."})]})})}),e(t,{children:e("div",{className:"w-full h-full bg-cover bg-center relative",style:{backgroundImage:"url(/images/auth/3.svg)"},children:a("div",{className:"absolute bottom-20 px-10",children:[e("p",{className:"text-neutral-50 text-3xl font-medium",children:"Waktunya Upgrade Pengalaman Audiomu!"}),e("p",{className:"text-neutral-50 text-base mt-2",children:"Nikmati penawaran eksklusif, rekomendasi khusus, dan kemudahan belanja perangkat audio terbaik hanya untukmu."})]})})})]}),e("style",{jsx:!0,global:!0,children:`
+                    .swiper-pagination {
+                        position: absolute;
+                        bottom: 10px;
+                        left: 0;
+                        width: 100%;
+                        text-align: left;
+                        padding-left: 40px;
+                    }
+                    .swiper-pagination-bullet {
+                        background-color: rgba(
+                            255,
+                            255,
+                            255,
+                            0.5
+                        );
+                        border-radius: 4px;
+                        height: 4px;
+                        opacity: 1; 
+                    }
+                    .swiper-pagination-bullet-active {
+                        background-color: rgba(
+                            255,
+                            255,
+                            255,
+                            1
+                        ); 
+                        width: 45px !important;
+                        border-radius: 4px;
+                        transition: width 0.3s ease, background-color 0.3s ease;
+                    }
+                `})]}),e("div",{className:"flex w-full lg:w-1/2 items-center justify-center bg-white",children:l})]})}export{c as A};
