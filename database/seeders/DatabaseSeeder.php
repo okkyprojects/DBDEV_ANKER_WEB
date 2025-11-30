f674d07ad8dd176d5f2079a45fe776d948ae0cd4
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2025-11-30 13:33:03 +0700

    fix spatie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,8 +33,8 @@ class DatabaseSeeder extends Seeder
         $this->call(LocationSeeder::class);
         $this->call(UserSeeder::class);
         User::factory()->count(5)->create();
-        Category::factory()->count(5)->create();
-        Brand::factory()->count(5)->create();
+        // Category::factory()->count(5)->create();
+        // Brand::factory()->count(5)->create();
         // Product::factory()->count(30)->create()->each(function ($product) {
         //     Variant::factory()->count(rand(1, 3))->create([
         //         'product_uuid' => $product->uuid,
